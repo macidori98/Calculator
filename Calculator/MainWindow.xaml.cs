408d577eaaba15bb464b909b0139e22803b5ca9b
Dorottya Szabó <macidori98@gmail.com>
2020-09-21 13:47:16 +0300

    click handlers created

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -7,9 +7,42 @@ namespace Calculator
   /// </summary>
   public partial class MainWindow : Window
   {
+    private double lastNumber, result;
+
     public MainWindow()
     {
       InitializeComponent();
+
+      this.acButton.Click += AcButton_Click;
+      this.negativeButton.Click += NegativeButton_Click;
+      this.percentageButton.Click += PercentageButton_Click;
+    }
+
+    private void PercentageButton_Click(object sender, RoutedEventArgs e)
+    {
+      if (this.resultLabel.Content.ToString() != "0")
+      {
+        if (double.TryParse(this.resultLabel.Content.ToString(), out lastNumber))
+        {
+          this.resultLabel.Content = (this.lastNumber / 100);
+        }
+      }
+    }
+
+    private void NegativeButton_Click(object sender, RoutedEventArgs e)
+    {
+      if (this.resultLabel.Content.ToString() != "0")
+      {
+        if (double.TryParse(this.resultLabel.Content.ToString(), out lastNumber))
+        {
+          this.resultLabel.Content = (this.lastNumber * (-1));
+        }
+      }
+    }
+
+    private void AcButton_Click(object sender, RoutedEventArgs e)
+    {
+      this.resultLabel.Content = "0";
     }
 
     private void sevenButton_Click(object sender, RoutedEventArgs e)
