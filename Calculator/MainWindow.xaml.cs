e70179f2c0ee33a4644eea1c2908486de9697da4
Dorottya Szabó <macidori98@gmail.com>
2020-09-21 13:23:49 +0300

    init

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Calculator
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void sevenButton_Click(object sender, RoutedEventArgs e)
+    {
+      if (this.resultLabel.Content.ToString() == "0")
+      {
+        this.resultLabel.Content = "7";
+      }
+      else
+      {
+        this.resultLabel.Content = $"{resultLabel.Content}7";
+      }
+    }
+  }
+}
